4fd64312d43374ac9c70ad254d1e0f9c050bc582
Cydo <cydoentis@gmail.com>
2022-07-21 13:57:51 -0400

    Added dark mode
    Added dark mode

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -2,18 +2,80 @@ const body = document.getElementsByTagName("body")[0];
 const h3 = document.getElementsByTagName("h3");
 const darkModeBtn = document.getElementById("darkMode");
 const sideNav = document.querySelector(".nav-side")
-const button = document.querySelector(".btn");
-const deskTopButton = document.querySelector(".btn-desktop");
+const buttons = document.querySelectorAll(".btn");
+const inputs = document.getElementsByTagName("input");
+const comments = document.querySelectorAll(".comment");
+const commentBodies = document.querySelectorAll(".comment-body");
+const textareas = document.getElementsByTagName('textarea');
 
+
+const darkModeOn = () => {
+  body.classList.add('dark');
+  sideNav.classList.add('dark-nav');
+
+  for(let button of buttons) {
+    button.classList.add('btn-dark');
+  }
+
+  for(let input of inputs) {
+    input.classList.add("input-dark");
+  }
+
+  for(let comment of comments) {
+    comment.classList.add("comment-dark");
+  }
+
+  for(let body of commentBodies) {
+    body.classList.add("body-dark");
+  }
+
+  for(let textarea of textareas) {
+    textarea.classList.add("input-dark");
+  }
+}
+
+const darkModeOff = () => {
+  body.classList.remove('dark');
+  sideNav.classList.remove('dark-nav');
+
+  for(let button of buttons) {
+    button.classList.remove('btn-dark');
+  }
+
+  for(let input of inputs) {
+    input.classList.remove("input-dark");
+  }
+
+  for(let comment of comments) {
+    comment.classList.remove("comment-dark");
+  }
+
+  for(let body of commentBodies) {
+    body.classList.remove("body-dark");
+  }
+
+  for(let textarea of textareas) {
+    textarea.classList.remove("input-dark");
+  }
+}
+
+window.onload = function() {
+  const isDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+  console.log(isDarkMode);
+  if(isDarkMode) {
+    darkModeOn();
+  } else if(localStorage.getItem('darkMode') === false){
+    darkModeOff();
+  }
+}
 
 darkModeBtn.addEventListener("click", () => {
-  body.classList.add('dark');
-  // h3.classList.add('dark-txt');
-  sideNav.style.backgroundColor = "#1E1E1E";
-  sideNav.style.border = "none";
-  button.style.backgroundColor = "white";
-  button.style.color = "black";
-  deskTopButton.style.backgroundColor = "white";
-  deskTopButton.style.color = "black";
-
+  const isDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+  if(isDarkMode) {
+    localStorage.setItem("darkMode", false);
+    darkModeOff();
+  } else {
+    localStorage.setItem("darkMode", true);
+    darkModeOn();
+  }
 })
